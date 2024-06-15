6031e4cb9f8344d3a2d501fedd8bec1d25ff5d9a
luccamaidana <99457796+luccamaidana@users.noreply.github.com>
2024-06-15 03:43:12 -0300

    Change background colour to black

--- rps.js
+++ rps.js
@@ -13,4 +13,29 @@ function getComputerChoice(){
 
 }
 
+function validChoice(op){
+    if (op === 1 || op === 2 || op === 3)
+        return op
+    
+    else
+        do{
+        op = parseInt(prompt(`\t\tERROR!\nCHOOSE:\n\n1- ROCK\n2- PAPER\n3- SCISSORS\n`))
+        } while (op !== 1 && op !== 2 && op !== 3)    
+        return op
+
+}
+
+function getHumanChoice (){
+    let x = parseInt(prompt(`CHOOSE:\n\n1- ROCK\n2- PAPER\n3- SCISSORS\n`))
+    x = validChoice(x)
+
+    if(x === 1)
+        return "ROCK"
+    else if (x === 2)
+        return "PAPER"
+    else if (x === 3)
+        return "SCISSORS"
+}
+
+getHumanChoice()
 getComputerChoice()

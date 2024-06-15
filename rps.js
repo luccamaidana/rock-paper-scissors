098f09a800aaca1fda0b4258ae301acb05d6fd4f
luccamaidana <99457796+luccamaidana@users.noreply.github.com>
2024-06-15 02:11:43 -0300

    Change error link to script

---
+++ rps.js
@@ -0,0 +1,20 @@
+#!Just want to do a comment line with this syntax
+
+function getComputerChoice(){
+    let x = Math.random()
+
+    switch(x){     
+        case (x===0):
+            console.log("ROCK")
+            break;
+        case (x===1):
+            console.log("ROCK")
+            break;
+        case (x!=0 && x!=1):
+            console.log("ROCK")
+            break;
+    }
+            
+}
+
+getComputerChoice()
